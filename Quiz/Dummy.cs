12a55cf42f8efa9e4887e7b2f61203d2bae77043
Clifford <clifford.agata@hotmail.com>
2017-11-18 01:53:17 +0100

    Add project files.

---
+++ Quiz/Dummy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz
+{
+    class Dummy
+    {
+        List<IQuestions> questions = new List<IQuestions>();
+
+        public Dummy()
+        {
+
+            OpenQuestion first = new OpenQuestion()
+            {
+                Moeilijkheidsgraad = 1,
+                Category = "mensen",
+                GetQuestion = "Hoe heet je ?",
+                GetAnswer = "cliff"
+            };
+            OpenQuestion tweede = new OpenQuestion()
+            {
+                Moeilijkheidsgraad = 2,
+                Category = "dieren",
+                GetQuestion = "hoe heet je hond?",
+                GetAnswer = "jego"
+            };
+            OpenQuestion derde = new OpenQuestion()
+            {
+                Moeilijkheidsgraad = 3,
+                Category = "mensen",
+                GetQuestion = "hoe oud ben je?",
+                GetAnswer = "24"
+            };
+            OpenQuestion vierde = new OpenQuestion()
+            {
+                Moeilijkheidsgraad = 1,
+                Category = "dingen",
+                GetQuestion = "Op welke locatie zit je op school?",
+                GetAnswer = "den haag"
+            };
+            questions.Add(first);
+            questions.Add(tweede);
+            questions.Add(derde);
+            questions.Add(vierde);
+        }
+
+        public List<IQuestions> GetList()
+        {
+            return questions;
+        }
+
+    }
+}
---
+++ Quiz/IQuestions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+   public interface IQuestions
+    {
+        int Moeilijkheidsgraad { get; set; }
+        String Category { get; set; }
+        String GetQuestion { get; set; }
+        String GetAnswer { get; set; }
+       
+        
+    }
+}
